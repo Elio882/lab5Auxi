fbe06b2c85da52ab7337072e4af3d4b4a1f32450
Mauricio Flores Herbas <mfloresherbas00@gmail.com>
2024-03-31 23:15:24 -0400

    feature(common):[added LAB-5]

--- src/homework/Q-04.ts
+++ src/homework/Q-04.ts
@@ -10,9 +10,24 @@
   //the function 2 does the same
 
   // EX - 2
-  interface Calculate{
+  interface VariablesArea{
+    length?:number,
+    width?:number, 
+    radius?: number
+  }
+   interface Calculate{
     calculate(length?: number, width?:number, radius?: number): number;
   }
+  function CalculateRectangle({ length, width }: VariablesArea): number{
+    return (length !== undefined && width !== undefined) ? length * width : 
+    (() => { throw new Error("Se deben proporcionar la longitud y el ancho del rectángulo."); })();
+  }
+
+  function calculateCircle({ radius }: VariablesArea): number {
+    return radius !== undefined ? Math.PI * radius * radius : 
+      (() => { throw new Error("Se debe proporcionar el radio del círculo."); })();
+  }
+
 
   class CalculateRectangleArea implements Calculate{
     calculate(length: number, width: number): number {
@@ -37,5 +52,22 @@
     //...some code for validate here
     return false;
   }
+
+  interface Validator<T, V> {
+    validateEmail(value: T): V;
+    validateUsername(value: T): V;
+}
+
+class CustomValidator implements Validator<string, boolean>{
+    validateEmail(value: string): boolean {
+        //...some code for validate here
+        return false;
+    }
+    validateUsername(value: string): boolean {
+        //...some code for validate here
+        return false;
+    }
+
+}
 
 })();
